df498d81eff9db4d7fcb756bba007a9ca08aef6c
unknown <denchik431@mail.ru>
2021-07-20 12:32:54 +0300

    jast_1 commit

--- app/models.py
+++ app/models.py
@@ -1,29 +1,34 @@
 from django.db import models
 from django.utils import timezone
 
+class DealsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().order_by('customer')
+
 class Deals(models.Model):
     customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
     item = models.TextField(max_length=50)
     total = models.FloatField(null=True, blank=True)
     quantity = models.FloatField(null=True, blank=True)
     date = models.DateTimeField(default=timezone.now)
+    objects = DealsManager()
             
     def __str__(self):
         return self.item
 
+class CustomerManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().order_by('username')
+
 class Customer(models.Model):
     username = models.TextField(max_length=30)
-    #spent_money = models.FloatField(null=True, blank=True)
+    spent_money = models.FloatField(null=True, blank=True)
     #gems = models.TextField(max_length=50)
+    objects = CustomerManager()
 
     def __str__(self):
         return self.username
 
-    '''def save(self, *args, **kwargs):
-        list_spent_money=[]
-        s = 0
-        list_spent_money.append(self.spent_money)
-        for m in list_spent_money:
-            s+=int(m)
-            self.spent_money = s
-            super(Customer, self).save(*args, **kwargs)'''
+    def save(self, *args, **kwargs):
+        self.spent_money+=self.spent_money
+        super(Customer, self).save(*args, **kwargs)
